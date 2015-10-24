18adbc6382d177fe054554648c727b73a04bae64
Grotup <nael.sylvain@gmail.com>
2015-10-25 00:18:46 +0200

    Suppression des services pour utiliser des fichiers de ressources

--- js/app.js
+++ js/app.js
@@ -1,59 +1,40 @@
 "use strict";
 angular.module('TonicApp', [])
 
-  .controller('TonicController', ["$scope", "TonicService", "$http", function($scope, TonicService, $http){
+  .controller('TonicController', ["$scope", "$http", function($scope, $http){
     $scope.chords = [];
 
     var allCards = [];
+    var allChords = [];
     $http.get('./resources/cards.json')
       .then(function(res){
         allCards = res.data;
-        $scope.randomizeCard();
+        $http.get('./resources/chords.json')
+          .then(function(res){
+            allChords = res.data;
+            $scope.randomizeCard();
+          });
       });
 
     $scope.randomizeCard = function(){
       $scope.chords = [];
-      $scope.actualCard = allCards[Math.floor(Math.random()*allCards.length)];
+      $scope.actualCard = getRandomCard();
       if($scope.actualCard.chords ){
-        $scope.chords = TonicService.getRandomChords($scope.actualCard.chords);
+        $scope.chords = getRandomChords($scope.actualCard.chords);
       }
     };
-  }])
 
-  .service('TonicService', function(){
-    var chords = ["A", "Am", "Bµ",
-                  "Bm", "C", "D",
-                  "Dm", "E", "Em",
-                  "F", "G"];
+    var getRandomCard = function(){
+      return allCards[Math.floor(Math.random()*allCards.length)];
+    };
 
-    var cards = [
-      {
-        "libelle" : "USE",
-        "description" : "Two notes. When you get bored, keep one and pick a new one. Repeat as long as you can.",
-        "img" : undefined,
-        "chords" : 2
-      },
-      {
-        "libelle" : "USE",
-        "description" : "Three notes until you get bored. Then pick three new ones. Repeat as long as you can.",
-        "img" : undefined,
-        "chords" : 3
-      },
-      {
-        "libelle" : "PLAY THIS GRAPH",
-        "description" : "The vertical axis is pitch. The horizontal is time. It should take about 2 minutes to play.",
-        "img" : undefined
-      }
-    ];
-
-		this.getRandomChords = function(iNbCards){
-      console.log(iNbCards);
+    var getRandomChords = function(iNbChords){
       var ret = [];
 
-			var tmpChords = chords.slice();
+      var tmpChords = allChords.slice();
       var randomIdx = -1;
-			for(var i = 0 ; i < iNbCards ; i++){
-        randomIdx = Math.floor(Math.random()*chords.length);
+      for(var i = 0 ; i < iNbChords ; i++){
+        randomIdx = Math.floor(Math.random()*allChords.length);
         if(ret.indexOf(tmpChords[randomIdx]) != -1){
           i++;
         }
@@ -61,18 +42,9 @@ angular.module('TonicApp', [])
           ret.push(tmpChords[randomIdx]);  
           // Suppression de l'élement sélectionné, pour éviter les doublons
           tmpChords.splice(randomIdx, 1);
-          console.log(tmpChords);
         }
-			}
+      }
 
       return ret;
-		};
-
-    this.randomChord = function(){
-      return chords[Math.floor(Math.random()*chords.length)];
     };
-
-    this.randomCard = function(){
-      return cards[Math.floor(Math.random()*cards.length)];
-    };
-  });
+  }]);
